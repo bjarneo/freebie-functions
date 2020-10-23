37a0f51d7e36299e3f86213833cb65e4e0e2d0fd
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2020-10-23 23:07:40 +0200

    refactor

--- functions/url.js
+++ functions/url.js
@@ -1,16 +1,5 @@
 const reverse = require('long-url');
 
-/*
-{
-    "path": "Path parameter",
-    "httpMethod": "Incoming request's method name"
-    "headers": {Incoming request headers}
-    "queryStringParameters": {query string parameters }
-    "body": "A JSON string of the request payload."
-    "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
-}
-*/
-
 async function longUrl({ body }) {
     const { url } = JSON.parse(body);
 
@@ -24,12 +13,12 @@ async function longUrl({ body }) {
     }
 
     return new Promise((resolve, reject) => {
-        reverse(url, function(err, url) {
+        reverse(url, function reverseUrl(err, url) {
             if (err) {
                 reject({
                     statusCode: 400,
                     body: JSON.stringify({
-                        error: 'Bad URL',
+                        error: 'Could not follow the URL',
                         url
                     })
                 });
@@ -44,11 +33,5 @@ async function longUrl({ body }) {
         });
     });
 }
-/*
-async function main() {
-    const res = await longUrl({ body: JSON.stringify({ url: 'http://ow.ly/W7oOu' }) });
-    console.log(res)
-}
-main()
-*/
+
 exports.handler = longUrl;
